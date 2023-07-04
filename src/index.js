265e6b617126d9c3cfa6ea1a6597a50813e235c9
Vitalii-Dashivets <vdashivets1985@gmail.com>
2023-07-04 12:45:30 +0300

    Add database

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 import './js/firebase-main.js';
-import './js/modal-auth.js';
+
 import './js/mobile-menu.js';
 import './js/dark.js';
 import './js/support.js';
@@ -10,3 +10,4 @@ import './js/modal-window.js';
 import './js/book.js';
 import './js/add-and-remove-book.js'
 import './js/shopping-list.js'
+import './js/modal-auth.js';
--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -2,7 +2,7 @@ import { dataUser } from './firebase-api.js';
 import amazon from '../img/shopping/amazon.png';
 import apple from '../img/shopping/apple.png';
 import bookshop from '../img/shopping/bookshop.png';
-
+import { onBtnInSelect, onBtnUpSelect, firebaseConfig, dataUser, authStates, writeUserData,readUserData,onLogout } from './firebase-api.js';
 
 const shopListEl = document.querySelector('.shopping-list-list');
 // dataUser.shoppingList.length === 4;
@@ -10,9 +10,30 @@ const shopListEl = document.querySelector('.shopping-list-list');
 // dataUser.shoppingList = [1];
 
 function shoppingListMarkup() {
-    if (dataUser.shoppingList.length === 0) {
+    readUserData(dataUser.userId).then(snapshot => {
+        if (snapshot.exists()) {
+            console.log('snap', snapshot.val());
         
-        const markup = `<li><p class="empty-shopping-list-text">
+            const { email, shoppingList, username } = snapshot.val();
+            console.log(shoppingList["-NZUy2Otr3H3ov2xTry5"]);
+        
+            console.log(Object.keys(shoppingList));
+            const keys = Object.keys(shoppingList);
+            if (keys.length !== 0) {
+                for (let key of keys) {
+             
+                    book = shoppingList[`${key}`];
+                    console.log(book);
+                }
+
+                if (authStates.status === true) {
+                    updateSignInButton(username);
+                }
+            }
+        }
+        })
+    
+            const markupDefaut = `<li><p class="empty-shopping-list-text">
       This page is empty, add some books and proceed to order.
     </p>
     <div>
@@ -37,110 +58,113 @@ function shoppingListMarkup() {
         />
       </picture>
     </div></li>`;
-        // shopListEl.insertAdjacentHTML('afterbegin', markup);
-        shopListEl.innerHTML = markup;
-    } else {
-        const markup = dataUser.shoppingList.map(book => {
-            `<li class="shop-cart-container">
-        <div class="shop-cart-wrap">
-          <div class="shop-image-wrapper">
-            <img class="shop-image" src="${book.book_image}" alt="${book.title}" width="100" height="142" />
-          </div>
-          <div class="shop-cart-info">
-            <h2 class="shop-cart-title">${book.title}</h2>
-            <h3 class="shop-cart-category">${book.list_name}</h3>
-            <p class="shop-cart-description">${book.description}
-            </p>
-            <div class="shop-cart-bottom-wrap">
-              <h4 class="shop-cart-author">${book.author}</h4>
-              <ul class="shop-cart-media">
-                <li class="shop-cart-media-item">
-                  <a
-                    href="${book.buy_links[0].url}"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    aria-label="Amazon"
-                  >
-                    <picture class="amazon-picture">
-                      <source
-                        srcset="${amazon}"
-                        type="image/png"
-                      />
-                      <img
-                        class="media-icon amazon-icon"
-                        src="${amazon}"
-                        alt="Amazon logo"
-                        width="32"
-                        height="11"
-                      />
-                    </picture>
-                  </a>
-                </li>
-                <li class="shop-cart-media-item">
-                  <a
-                    href="${book.buy_links[1].url}"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    aria-label="Apple Books"
-                  >
-                    <picture>
-                      <source
-                        srcset="${apple}"
-                        type="image/png"
-                      />
-                      <img
-                        class="media-icon media-book-icon"
-                        src="${apple}"
-                        alt="Apple book logo"
-                        width="16"
-                        height="16"
-                      />
-                    </picture>
-                  </a>
-                </li>
-                <li class="shop-cart-media-item">
-                  <a
-                    href="${book.buy_links[4].url}"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    aria-label="Bookshop"
-                  >
-                    <picture>
-                      <source
-                        srcset="${bookshop}"
-                        type="image/png"
-                      />
-                      <img
-                        class="media-icon media-book-icon"
-                        src="${bookshop}"
-                        alt="Book shop logo"
-                        width="16"
-                        height="16"
-                      />
-                    </picture>
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <button
-              class="shop-cart-btn"
-              type="button"
-              data-title="title"
-              aria-label="Remove button"
-            >
-              <svg class="shop-cart-btn-trash">
-                <use href="../img/icons.svg#icon-trash"></use>
-              </svg>
-            </button>
-          </div>
-        </div>
-      </li>`
+            // shopListEl.insertAdjacentHTML('afterbegin', markup);
+          
+      
+    //         const markupList = data.shoppingList.map(book => {
+    //             `<li class="shop-cart-container">
+    //     <div class="shop-cart-wrap">
+    //       <div class="shop-image-wrapper">
+    //         <img class="shop-image" src="${book.book_image}" alt="${book.title}" width="100" height="142" />
+    //       </div>
+    //       <div class="shop-cart-info">
+    //         <h2 class="shop-cart-title">${book.title}</h2>
+    //         <h3 class="shop-cart-category">${book.list_name}</h3>
+    //         <p class="shop-cart-description">${book.description}
+    //         </p>
+    //         <div class="shop-cart-bottom-wrap">
+    //           <h4 class="shop-cart-author">${book.author}</h4>
+    //           <ul class="shop-cart-media">
+    //             <li class="shop-cart-media-item">
+    //               <a
+    //                 href="${book.buy_links[0].url}"
+    //                 target="_blank"
+    //                 rel="noopener noreferrer"
+    //                 aria-label="Amazon"
+    //               >
+    //                 <picture class="amazon-picture">
+    //                   <source
+    //                     srcset="${amazon}"
+    //                     type="image/png"
+    //                   />
+    //                   <img
+    //                     class="media-icon amazon-icon"
+    //                     src="${amazon}"
+    //                     alt="Amazon logo"
+    //                     width="32"
+    //                     height="11"
+    //                   />
+    //                 </picture>
+    //               </a>
+    //             </li>
+    //             <li class="shop-cart-media-item">
+    //               <a
+    //                 href="${book.buy_links[1].url}"
+    //                 target="_blank"
+    //                 rel="noopener noreferrer"
+    //                 aria-label="Apple Books"
+    //               >
+    //                 <picture>
+    //                   <source
+    //                     srcset="${apple}"
+    //                     type="image/png"
+    //                   />
+    //                   <img
+    //                     class="media-icon media-book-icon"
+    //                     src="${apple}"
+    //                     alt="Apple book logo"
+    //                     width="16"
+    //                     height="16"
+    //                   />
+    //                 </picture>
+    //               </a>
+    //             </li>
+    //             <li class="shop-cart-media-item">
+    //               <a
+    //                 href="${book.buy_links[4].url}"
+    //                 target="_blank"
+    //                 rel="noopener noreferrer"
+    //                 aria-label="Bookshop"
+    //               >
+    //                 <picture>
+    //                   <source
+    //                     srcset="${bookshop}"
+    //                     type="image/png"
+    //                   />
+    //                   <img
+    //                     class="media-icon media-book-icon"
+    //                     src="${bookshop}"
+    //                     alt="Book shop logo"
+    //                     width="16"
+    //                     height="16"
+    //                   />
+    //                 </picture>
+    //               </a>
+    //             </li>
+    //           </ul>
+    //         </div>
+    //         <button
+    //           class="shop-cart-btn"
+    //           type="button"
+    //           data-title="title"
+    //           aria-label="Remove button"
+    //         >
+    //           <svg class="shop-cart-btn-trash">
+    //             <use href="../img/icons.svg#icon-trash"></use>
+    //           </svg>
+    //         </button>
+    //       </div>
+    //     </div>
+    //   </li>`
                       
-        }).join("");
-        shopListEl.innerHTML = markup;
-        
+    //         }).join("");
+            
+       
+    
+    
+    
     }
-}
+
 
 export {shoppingListMarkup};
 
