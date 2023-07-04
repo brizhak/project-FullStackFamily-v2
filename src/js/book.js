8dc8990ba29812912df78c2498404c01e734fa5b
kika88vk <69463446+kika88vk@users.noreply.github.com>
2023-07-04 23:22:55 +0300

    Update book.js

--- src/js/book.js
+++ src/js/book.js
@@ -9,39 +9,20 @@ import bookshop from '../img/shopping/bookshop.png';
 const addToCartBtn = document.querySelector('#add-to-cart-btn');
 const removeFromCartBtn = document.querySelector('#remove-from-cart-btn');
 import { authStates } from './firebase-api';
-
 import { showLoader, hideLoader } from './loader';
 
 
 let modalBodyCard = document.querySelector('.modal-body-card');
 const booksCategoryEl = document.querySelector('.books-category');
-booksCategoryEl.addEventListener('click', onSelectBook);
-function onSelectBook(evt) {
 
+booksCategoryEl.addEventListener('click', onSelectBook);
+
+
+function onSelectBook(evt) {
     // showLoader();
     let touchTagA = evt.target.closest('a');
 
-
-  let touchTagA = evt.target.closest('a');
-  if (!touchTagA) return;
-  if (!booksCategoryEl.contains(touchTagA)) return;
-  console.log(touchTagA.id);
-  openModal();
-  modalBodyCard.innerHTML = '';
-  let id = touchTagA.id;
-  getBook(id);
-  // if (authStates.status === true) {
-  //     addToCartBtn.disabled = false;
-  //     removeFromCartBtn.disabled = false;
-  //     addToCartBtn.classList.remove('non-active-btn');
-  //     removeFromCartBtn.classList.remove('non-active-btn');
-  // } else {
-  //     addToCartBtn.disabled = true;
-  //     removeFromCartBtn.disabled = true;
-  //     addToCartBtn.classList.add('non-active-btn');
-  //     removeFromCartBtn.classList.add('non-active-btn');
-  // }
-
+    if (!touchTagA) return;
 
     if (!booksCategoryEl.contains(touchTagA)) return;
 
@@ -64,19 +45,24 @@ function onSelectBook(evt) {
         removeFromCartBtn.classList.add('non-active-btn');
     }
     // hideLoader();
+}
 
-}
+
+
 let book = null;
+
 async function getBook(id) {
-  try {
-    book = await fetchSelectedBook(id);
-    renderSelectedBook(book);
-  } catch (error) {
-    Notiflix.Notify.failure('Something went wrong. Please try again');
-  }
+    try {
+        book = await fetchSelectedBook(id);
+        renderSelectedBook(book);
+    } catch (error) {
+        Notiflix.Notify.failure('Something went wrong. Please try again');
+    }
 }
+
+
 function renderSelectedBook(book) {
-  const markup = `
+    const markup = `
         <div class="modal-body-image">
             <img
                 src="${book.book_image}"
@@ -86,13 +72,13 @@ function renderSelectedBook(book) {
         </div>
         <div class="modal-body-tablet">
             <div class="modal-body-about-book">
-            <h2 class="modal-body-title">${book.title}</h2>
-            <h3 class="modal-body-autor">${book.author}</h3>
-            <p class="modal-body-text">
-                ${book.description}
-            </p>
-        </div>
-        <div class="modal-body-logo">
+                <h2 class="modal-body-title">${book.title}</h2>
+                <h3 class="modal-body-autor">${book.author}</h3>
+                <p class="modal-body-text">
+                    ${book.description}
+                </p>
+            </div>
+            <div class="modal-body-logo">
             <ul class="modal-body-logo-list">
                 <li class="modal-body-logo-item">
                 <a
@@ -139,13 +125,15 @@ function renderSelectedBook(book) {
                         srcset="${bookshop}";
                         src="${bookshop}";
                         alt="Bookshop logo"
-                        />
+                        /> 
                 </a>
                 </li>
             </ul>
         </div>
-        </div>
-        `;
-  modalBodyCard.insertAdjacentHTML('afterbegin', markup);
+        </div>`;
+
+
+    modalBodyCard.insertAdjacentHTML("afterbegin", markup);
 }
+
 export { book };
