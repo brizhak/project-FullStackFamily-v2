3dac31150ab3bc9eacf80d76b58ac64b87c4fce8
Ushakov Aleksandr <122167617+Ushakov3@users.noreply.github.com>
2023-07-05 21:36:17 +0300

    update

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -2,14 +2,18 @@ let modalWrapper = document.querySelector('.modal');
 let modalOverlay = document.querySelector('.overlay');
 let closeModalBtn = document.querySelector('.modal-close-btn');
 
+let body = document.querySelector('body');
+
 export function openModal() {
   modalWrapper.classList.add('active');
   modalOverlay.classList.add('active');
+  body.style.overflow = 'hidden';
 }
 
 function closeModal() {
   modalWrapper.classList.remove('active');
   modalOverlay.classList.remove('active');
+  body.style.overflow = 'auto';
 }
 
 closeModalBtn.addEventListener('click', closeModal);
