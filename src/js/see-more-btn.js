683cd6216a9132ed2bf771de33582c94c0ca8779
Artur Myronchuk <rolax223@gmail.com>
2023-07-05 21:24:37 +0300

    str

--- src/js/see-more-btn.js
+++ src/js/see-more-btn.js
@@ -1,4 +1,4 @@
-import { fetchCertainCategory } from "./api_request";
+import { fetchCertainCategory } from './api_request';
 const seeMoreBtn = document.querySelector('.see-more');
 const booksCategoryEl = document.querySelector('.books-category');
 import { showLoader, hideLoader } from './loader';
@@ -6,41 +6,38 @@ import Notiflix from 'notiflix';
 
 booksCategoryEl.addEventListener('click', onOpenCategoryList);
 
-let category = "";
+let category = '';
 
 async function onOpenCategoryList(evt) {
-
-    try {
-        if (evt.target.nodeName !== "BUTTON") {
-            return;
-        }
-        category = evt.target.id;
-        console.log(evt.target);
-
-        let books = await fetchCertainCategory(category);
-        if (!books) {
-            Notiflix.Notify.failure("Ops! We don't have books certain category");
-        }
-        renderCurrentCategory(books);
-        return category;
-    } catch (error) {
-        Notiflix.Notify.failure('Something went wrong. Please try again');
-        console.log(error);
-    } finally {
-
+  try {
+    if (evt.target.nodeName !== 'BUTTON') {
+      return;
     }
+    category = evt.target.id;
+    console.log(evt.target);
+
+    let books = await fetchCertainCategory(category);
+    if (!books) {
+      Notiflix.Notify.failure("Ops! We don't have books certain category");
+    }
+    renderCurrentCategory(books);
+    return category;
+  } catch (error) {
+    Notiflix.Notify.failure('Something went wrong. Please try again');
+    console.log(error);
+  } finally {
+  }
 }
 
-
-
 function renderCurrentCategory(arr) {
-    const booksTop = document.createElement('div');
-    booksTop.classList.add('list-top-books')
-    let markupCatBook = arr
-        .map(({ book_image, author, title, _id, list_name }, index) => {
-            const bookTitleClass = index === 0 ? 'best-sellers-title' : 'visible-hidden-title';
+  const booksTop = document.createElement('div');
+  booksTop.classList.add('list-top-books');
+  let markupCatBook = arr
+    .map(({ book_image, author, title, _id, list_name }, index) => {
+      const bookTitleClass =
+        index === 0 ? 'best-sellers-title' : 'visible-hidden-title';
 
-            return `
+      return `
                     <a href="#" class="book-card" id="${_id}">
                     <p class="${bookTitleClass}">${list_name}</p>
                     <div class="book-carts">
@@ -54,23 +51,18 @@ function renderCurrentCategory(arr) {
                     </div>
                     </a>
         `;
-        }).join('');
+    })
+    .join('');
 
+  let listCategoryBook = document.querySelector('.category-item-list');
+  // listCategoryBook.insertAdjacentHTML('beforeend', markup);
+  // listCategoryBook.innerHTML = markupCatBook;
+  booksTop.innerHTML = markupCatBook;
+  listCategoryBook.id = category;
 
+  if (listCategoryBook.id === category) {
+    listCategoryBook.innerHTML = '';
 
-    let listCategoryBook = document.querySelector('.category-item-list')
-    // listCategoryBook.insertAdjacentHTML('beforeend', markup);
-    // listCategoryBook.innerHTML = markupCatBook;
-    booksTop.innerHTML = markupCatBook;
-    listCategoryBook.id = category;
-
-    if (listCategoryBook.id === category) {
-        listCategoryBook.innerHTML = '';
-
-        listCategoryBook.appendChild(booksTop);
-    }
-
-
-
-
+    listCategoryBook.appendChild(booksTop);
+  }
 }
