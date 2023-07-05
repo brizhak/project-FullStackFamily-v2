992b5c0d19daad851590e21c40c2ae1cd1cbf76d
AnonymousMar <119819802+AnonymousMar@users.noreply.github.com>
2023-07-05 16:08:38 +0200

    dark theme

--- src/js/dark.js
+++ src/js/dark.js
@@ -1,6 +1,16 @@
 var content = document.getElementsByTagName('body')[0];
 var darkMode = document.getElementById('theme-switch');
+
 darkMode.addEventListener('click', function () {
     darkMode.classList.toggle('active');
     content.classList.toggle('night');
 })
+
+function darkMode() {
+    const wasDarkmode = localStorage.getItem('darkmode') == true
+
+    localStorage.setItem('darkmode', !wasDarkmode)
+    localStorage.toggle('active', !wasDarkmode)
+}
+
+document.addEventListener('DOMContentLoaded', onload)
