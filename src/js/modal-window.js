a09244c982104a1b343837b3cb4734644529929a
kika88vk <69463446+kika88vk@users.noreply.github.com>
2023-07-06 17:48:35 +0300

    add unscrolling modal

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -1,6 +1,6 @@
 import { reauthenticateWithPhoneNumber } from "firebase/auth";
 import { book } from "./book";
-import { dataUser, readShoppingList ,authStates} from './firebase-api';
+import { dataUser, readShoppingList, authStates } from './firebase-api';
 
 
 let modalWrapper = document.querySelector('.modal');
@@ -13,37 +13,41 @@ const removeFromCartBtn = document.querySelector('#remove-from-cart-btn');
 export function openModal() {
   modalWrapper.classList.add('active');
   modalOverlay.classList.add('active');
- 
+
+  const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+  const body = document.body;
+  body.style.position = 'fixed';
+  body.style.top = `-${scrollY}`;
 
   if (authStates.status === false) {
     return;
   }
-   document.body.addEventListener('keyup', escapeListener);
+  document.body.addEventListener('keyup', escapeListener);
   readShoppingList(dataUser.userId).then(snapshot => {
     if (snapshot.exists()) {
       const shoppingList = snapshot.val();
       const keys = Object.keys(shoppingList);
-             const books = [];
-        if (keys.length !== 0) {
-          for (let key of keys) {
-            const onebook = shoppingList[`${key}`];
-            books.push(onebook);
-           
-          }
-        
-          const result = books.find(elem =>
-            
-            elem._id === book._id
-          );
-          
-          if (result === undefined) {
-            addToCartBtn.classList.remove('visually-hidden');
-             removeFromCartBtn.classList.add('visually-hidden');
-          } else {
-            addToCartBtn.classList.add('visually-hidden');
-             removeFromCartBtn.classList.remove('visually-hidden');
-          }
-        
+      const books = [];
+      if (keys.length !== 0) {
+        for (let key of keys) {
+          const onebook = shoppingList[`${key}`];
+          books.push(onebook);
+
+        }
+
+        const result = books.find(elem =>
+
+          elem._id === book._id
+        );
+
+        if (result === undefined) {
+          addToCartBtn.classList.remove('visually-hidden');
+          removeFromCartBtn.classList.add('visually-hidden');
+        } else {
+          addToCartBtn.classList.add('visually-hidden');
+          removeFromCartBtn.classList.remove('visually-hidden');
+        }
+
       }
     }
   })
@@ -51,12 +55,23 @@ export function openModal() {
 }
 
 function closeModal() {
+
+  const body = document.body;
+  const scrollY = body.style.top;
+  body.style.position = '';
+  body.style.top = '';
+  window.scrollTo(0, parseInt(scrollY || '0') * -1);
+
   modalWrapper.classList.remove('active');
   modalOverlay.classList.remove('active');
 }
 
 closeModalBtn.addEventListener('click', closeModal);
 modalOverlay.addEventListener('click', closeModal);
+
+window.addEventListener('scroll', () => {
+  document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
+});
 // document.body.addEventListener('keyup', function (e) {
 //   const key = e.code;
 
@@ -72,4 +87,4 @@ function escapeListener(e) {
     document.body.removeEventListener('keyup', esapeListener);
   }
 }
-export{closeModal}
+export { closeModal }
