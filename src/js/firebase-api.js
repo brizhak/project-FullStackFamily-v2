a0bac38756d0190f857ce008939d09ae9a72f59b
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-07-06 03:40:24 +0300

    modal auth
    add view password
    hide Name log in

--- src/js/firebase-api.js
+++ src/js/firebase-api.js
@@ -1,9 +1,16 @@
-
-import {  refsBtn} from "./firebase-main";
-import { getDatabase, ref, set, child, get, update,push } from 'firebase/database'; 
+import { refsBtn } from './firebase-main';
+import {
+  getDatabase,
+  ref,
+  set,
+  child,
+  get,
+  update,
+  push,
+} from 'firebase/database';
 import { getAuth, signOut } from 'firebase/auth';
-import {resetSignInButton} from './mobile-menu.js'
-import { updateSignInButton } from './mobile-menu.js'
+import { resetSignInButton } from './mobile-menu.js';
+import { updateSignInButton } from './mobile-menu.js';
 
 const dataUser = {
   userId: '',
@@ -32,11 +39,13 @@ function onBtnInSelect() {
   }
 
   authStates.type = 'signin';
-  refsBtn.formBtn.textContent = 'SIGN IN';
+  refsBtn.formBtn.textContent = 'sign in';
   refsBtn.btnIn.style.color = 'blue';
   refsBtn.btnIn.style.textDecoration = 'underline';
   refsBtn.btnUp.style.color = '';
   refsBtn.btnUp.style.textDecoration = 'none';
+  refsBtn.form.name.value = '';
+  refsBtn.form.name.disabled = true;
 }
 
 function onBtnUpSelect() {
@@ -45,11 +54,12 @@ function onBtnUpSelect() {
   }
 
   authStates.type = 'signup';
-  refsBtn.formBtn.textContent = 'SIGN UP';
+  refsBtn.formBtn.textContent = 'sign up';
   refsBtn.btnUp.style.color = 'blue';
   refsBtn.btnUp.style.textDecoration = 'underline';
   refsBtn.btnIn.style.color = '';
   refsBtn.btnIn.style.textDecoration = 'none';
+  refsBtn.form.name.disabled = false;
 }
 
 async function writeUserData({ userId, username, email, shoppingList }) {
@@ -65,56 +75,57 @@ async function readUserData(userId) {
   const dbRef = ref(getDatabase());
 
   return get(child(dbRef, `users/${userId}`));
-    
 }
 
-
-async function updateUserData( data, userId ) {
+async function updateUserData(data, userId) {
   const db = getDatabase();
-  
-        const newPostKey = push(child(ref(db), `users/${userId}+/shoppingList/`)).key;
-        const updates = {};
-        updates['users/' + userId + '/shoppingList/'+newPostKey] = data;
-        console.log('updates: ', updates);
-        return update(ref(db), updates);
-      }   
 
-async function onLogout() {
-
-  const auth = getAuth();
-  signOut(auth).then(() => {
-  authStates.status = false;
-  resetSignInButton();
-  
-}).catch((error) => {
-  
-});
-}
-
-async function readShoppingList(userId) {
- const dbRef = ref(getDatabase());
- return  get(child(dbRef, `users/${userId}/shoppingList`));
-    
-}
-
-async function pushShoppingList(userId,data) {
-  const db = getDatabase();    
-  set(ref(db, 'users/' + userId+'/shoppingList/'),data  );
-  
-      }
-       
-    
-
-async function removeElShoppingList(userId, elemId) {
-  const db = getDatabase();
+  const newPostKey = push(child(ref(db), `users/${userId}+/shoppingList/`)).key;
   const updates = {};
-  updates['users/' + userId + '/shoppingList/'+elemId] = null;
+  updates['users/' + userId + '/shoppingList/' + newPostKey] = data;
   console.log('updates: ', updates);
   return update(ref(db), updates);
 }
 
+async function onLogout() {
+  const auth = getAuth();
+  signOut(auth)
+    .then(() => {
+      authStates.status = false;
+      resetSignInButton();
+    })
+    .catch(error => {});
+}
 
-export { onBtnInSelect, onBtnUpSelect, firebaseConfig, dataUser,authStates , writeUserData,readUserData,updateUserData,onLogout,pushShoppingList,readShoppingList,removeElShoppingList};
+async function readShoppingList(userId) {
+  const dbRef = ref(getDatabase());
+  return get(child(dbRef, `users/${userId}/shoppingList`));
+}
 
+async function pushShoppingList(userId, data) {
+  const db = getDatabase();
+  set(ref(db, 'users/' + userId + '/shoppingList/'), data);
+}
 
+async function removeElShoppingList(userId, elemId) {
+  const db = getDatabase();
+  const updates = {};
+  updates['users/' + userId + '/shoppingList/' + elemId] = null;
+  console.log('updates: ', updates);
+  return update(ref(db), updates);
+}
 
+export {
+  onBtnInSelect,
+  onBtnUpSelect,
+  firebaseConfig,
+  dataUser,
+  authStates,
+  writeUserData,
+  readUserData,
+  updateUserData,
+  onLogout,
+  pushShoppingList,
+  readShoppingList,
+  removeElShoppingList,
+};
