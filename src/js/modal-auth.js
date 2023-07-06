9aeee4e5adfae24cd507793407bb06b2add99b5c
kika88vk <69463446+kika88vk@users.noreply.github.com>
2023-07-06 18:25:01 +0300

    scroll-modal-auth

--- src/js/modal-auth.js
+++ src/js/modal-auth.js
@@ -20,14 +20,30 @@ function openModalAuth() {
 
   modalAuth.classList.add('active');
   overlayAuth.classList.add('active');
+
+  const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+  const body = document.body;
+  body.style.position = 'fixed';
+  // body.style.top = `-${scrollY}`;
 }
 
 function closeModalAuth() {
+
+  const body = document.body;
+  const scrollY = body.style.top;
+  body.style.position = '';
+  body.style.top = '';
+  window.scrollTo(0, parseInt(scrollY || '0') * -1);
+
   modalAuth.classList.remove('active');
   overlayAuth.classList.remove('active');
 }
 
 document.body.addEventListener('keyup', closeModalEscape);
+
+window.addEventListener('scroll', () => {
+  document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
+});
 
 function closeModalEscape(e) {
   if (e.code === 'Escape') {
@@ -58,7 +74,7 @@ function togglePassword() {
     togglePasswordButton.setAttribute(
       'aria-label',
       'Show password as plain text. ' +
-        'Warning: this will display your password on the screen.'
+      'Warning: this will display your password on the screen.'
     );
   } else {
     passwordInput.type = 'password';
